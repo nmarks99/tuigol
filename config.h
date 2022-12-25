5566ccaa41daf71709a1d23d2297a50bd1278e43
Nick Marks <nmarks99@gmail.com>
2022-12-25 12:53:47 -0600

    update comment

--- config.h
+++ config.h
@@ -8,7 +8,6 @@
    ██║   ╚██████╔╝██║╚██████╔╝╚██████╔╝███████╗
    ╚═╝    ╚═════╝ ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝
 
-
 Conway's Game of Life in your Terminal!
 
 This configuration file defines various parmeters for the program 
@@ -17,7 +16,7 @@ which you can change as desired.
 */
 
 
-#define GAME_SIZE 0.8   // percentage of terminal window to take up with game
+#define GAME_SIZE 0.8   // percentage of terminal window height to take up with the game
 #define ALIVE_CHAR 'o'  // character representing living cells
 #define DEAD_CHAR ' '   // character representing dead cells
 #define REFRESH_RATE 20 // rate at which to refresh the game
