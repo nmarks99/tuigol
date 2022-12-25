595eeb0d93669da643ff96ab8d2a1188c0597ad9
Nick Marks <nmarks99@gmail.com>
2022-12-25 12:52:12 -0600

    add config.h user configuration file

---
+++ config.h
@@ -0,0 +1,23 @@
+/*
+                                                                              
+
+████████╗██╗   ██╗██╗ ██████╗  ██████╗ ██╗     
+╚══██╔══╝██║   ██║██║██╔════╝ ██╔═══██╗██║     
+   ██║   ██║   ██║██║██║  ███╗██║   ██║██║     
+   ██║   ██║   ██║██║██║   ██║██║   ██║██║     
+   ██║   ╚██████╔╝██║╚██████╔╝╚██████╔╝███████╗
+   ╚═╝    ╚═════╝ ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝
+
+
+Conway's Game of Life in your Terminal!
+
+This configuration file defines various parmeters for the program 
+which you can change as desired.
+
+*/
+
+
+#define GAME_SIZE 0.8   // percentage of terminal window to take up with game
+#define ALIVE_CHAR 'o'  // character representing living cells
+#define DEAD_CHAR ' '   // character representing dead cells
+#define REFRESH_RATE 20 // rate at which to refresh the game
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,35 @@
 #include "gol.h"
 #include "graphics.h"
 
+#define USEC 1000000  // microseconds in a second
+#define ITERATIONS 100
+
 
 int main(int argc, char **argv) {
    
     // initialize graphics 
     Graphics gr; 
-
+    
+    // Define the game window and draw it
     int rows = gr.bdr_height;
-    Universe uni(rows,rows,true);
-
+    Universe uni(rows,rows,true); 
     gr.draw_universe(uni);
-    getch();
+    getch(); // Wait for user to press a key to start the game
+    
 
-    int iterations = 200;
+    // Main loop:
+    // Draw universe, get next generation according to rules, repeat
     int k = 0;
-    while (k < iterations) {
-        
+    while (k < ITERATIONS) {
         gr.draw_universe(uni);
         usleep(1000000/REFRESH_RATE);
         uni.next_gen();
-
         k++;
-
     }
-
+    
+    // Wait for user to press a key, then kill the program
     getch();
     gr.destroy();
-
-
 
     return 0;
 }
